5f802c5ee8b32d744da511e975d4b1e887d7291e
mirkobrignoli <127690671+mirkobrignoli@users.noreply.github.com>
2023-03-28 01:32:17 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
-
 int main(){
 //Qui vanno definite le variabili
-
+float a = 0, b = 0, c = 0;
 cin >> a >> b >> c;
-
 //Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+cout << a*b/2 << "\n";
+cout << a*a << "\n";
+cout << a*b << "\n";
+cout << (a + b)*c/2 << "\n";
+return 0;
 }
